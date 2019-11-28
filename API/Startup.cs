6ce4b232c81ccff09134af01e1dc6d63ff25f04d
Alexander <alexander.popov@quadecco.com>
2019-11-28 14:02:18 +0200

    End of Section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -30,6 +30,13 @@ namespace API
             services.AddDbContext<DataContext>(opt=>{
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                });
+            });
             services.AddControllers();
         }
 
@@ -40,13 +47,16 @@ namespace API
             {
                 app.UseDeveloperExceptionPage();
             }
+            else{
+
+            }
 
             // app.UseHttpsRedirection();
 
             app.UseRouting();
 
-            app.UseAuthorization();
-
+            //app.UseAuthorization();
+            app.UseCors("CorsPolicy");
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();

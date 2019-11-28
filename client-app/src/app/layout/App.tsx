a378ff36ed57284c2aa46a5f6f99af0f4626dae5
Oleksandr Popov <sanprof86@gmail.com>
2019-11-28 23:25:42 +0200

    Added NavBar component

---
+++ client-app/src/app/layout/App.tsx
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from "react";
+import { Header, Icon, List } from "semantic-ui-react";
+import "./styles.css";
+import axios from "axios";
+import { IActivity } from "../models/activity";
+import { NavBar } from "../../features/Nav/NavBar";
+
+const App = () => {
+  const [activities, setActivities] = useState<IActivity[]>([]);
+
+  useEffect(() => {
+    axios
+      .get<IActivity[]>("http://localhost:5000/api/activities")
+      .then(response => {
+        setActivities(response.data);
+      });
+  }, []);
+
+  return (
+    <div>
+      <NavBar />
+      <List>
+        {activities.map(activity => (
+          <List.Item key={activity.id}>{activity.title}</List.Item>
+        ))}
+      </List>
+    </div>
+  );
+};
+
+export default App;

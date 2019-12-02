8a5e1888dc640ab054ada60886762797c5017dba
Alexander <alexander.popov@quadecco.com>
2019-12-02 12:21:36 +0200

    End of section 6

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, Fragment } from "react";
+import React, { useState, useEffect, Fragment, SyntheticEvent } from "react";
 import { Container } from "semantic-ui-react";
 import "./styles.css";
 import { IActivity } from "../models/activity";
@@ -14,6 +14,8 @@ const App = () => {
     );
     const [editMode, setEditMode] = useState(false);
     const [loading, setLoading] = useState(true);
+    const [submitting, setSubmitting] = useState(false);
+    const [target, setTarget] = useState("");
 
     const handleSelectedActivity = (id: string) => {
         setSelectedActivity(activities.filter(a => a.id === id)[0]);
@@ -26,28 +28,41 @@ const App = () => {
     };
 
     const handleCreateActivity = (activity: IActivity) => {
-        agent.Activities.create(activity).then(() => {
-            setActivities([...activities, activity]);
-            setSelectedActivity(activity);
-            setEditMode(false);
-        });
+        setSubmitting(true);
+        agent.Activities.create(activity)
+            .then(() => {
+                setActivities([...activities, activity]);
+                setSelectedActivity(activity);
+                setEditMode(false);
+            })
+            .then(() => setSubmitting(false));
     };
 
     const handleEditActivity = (activity: IActivity) => {
-        agent.Activities.update(activity).then(() => {
-            setActivities([
-                ...activities.filter(a => a.id !== activity.id),
-                activity,
-            ]);
-            setSelectedActivity(activity);
-            setEditMode(false);
-        });
+        setSubmitting(true);
+        agent.Activities.update(activity)
+            .then(() => {
+                setActivities([
+                    ...activities.filter(a => a.id !== activity.id),
+                    activity,
+                ]);
+                setSelectedActivity(activity);
+                setEditMode(false);
+            })
+            .then(() => setSubmitting(false));
     };
 
-    const handleDeleteActivity = (id: string) => {
-        agent.Activities.delete(id).then(() => {
-            setActivities([...activities.filter(a => a.id !== id)]);
-        });
+    const handleDeleteActivity = (
+        event: SyntheticEvent<HTMLButtonElement>,
+        id: string
+    ) => {
+        setSubmitting(true);
+        setTarget(event.currentTarget.name);
+        agent.Activities.delete(id)
+            .then(() => {
+                setActivities([...activities.filter(a => a.id !== id)]);
+            })
+            .then(() => setSubmitting(false));
     };
 
     useEffect(() => {
@@ -81,6 +96,8 @@ const App = () => {
                     createActivity={handleCreateActivity}
                     editActivity={handleEditActivity}
                     deleteActivity={handleDeleteActivity}
+                    submitting={submitting}
+                    target={target}
                 />
             </Container>
         </Fragment>
